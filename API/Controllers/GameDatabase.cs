00d5db507859bbda9f971b77412eaabd5c1ee98e
Asger <asger-gjelstrup@hotmail.com>
2020-09-03 09:12:23 +0200

    Added API project

---
+++ API/Controllers/GameDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    //api/gamedatabase/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameDatabase : ControllerBase
+    {
+        // GET: api/<GameDatabase>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GameDatabase>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"{id}";
+        }
+
+        // POST api/<GameDatabase>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GameDatabase>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GameDatabase>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GameAPIBackend/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameAPILibrary
+{
+    public class Game
+    {
+
+    }
+}
